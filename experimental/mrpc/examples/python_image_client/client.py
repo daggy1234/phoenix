2b3a5712b97ac94a2dc28cae7043429f66a52a6c
Ubuntu <ubuntu@ip-172-31-18-70.ec2.internal>
2023-07-28 16:56:35 +0000

    a

--- experimental/mrpc/examples/python_image_client/client.py
+++ experimental/mrpc/examples/python_image_client/client.py
@@ -4,19 +4,32 @@ import csv
 from PIL import Image
 from io import BytesIO
 
-c = Connection("0.0.0.0:5000")
+time_ls = []
 with open("tree.jpg", "rb") as f:
     byt = f.read()
-start = time.time()
-req = ImageRequest(byt)
-o = c.icon_image(req)
-bio = BytesIO(o.data)
-im = Image.open(bio)
-im.save("icon.png")
-# with open("grayscale.png", "wb") as f:
-#     f.write(o.data)
-end = time.time()
-diff = end - start
-print(diff)
+c = Connection("172.31.27.226:5000")
+for i in range(1000):
+    start_g = time.time()
+    req_g = ImageRequest(byt)
+    o = c.grayscale_image(req_g)
+    end_g = time.time()
+
+
+    start_i = time.time()
+    req_i = ImageRequest(byt)
+    o = c.invert_image(req_i)
+    end_i = time.time()
+
+
+    start_ic = time.time()
+    req_ic = ImageRequest(byt)
+    o = c.icon_image(req_ic)
+    end_ic = time.time()
+
+    time_ls.append([str(i), end_g - start_g, end_i - start_i, end_ic - start_ic])
+
+with open("mrpc_data.csv", "w") as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerows(time_ls)
 
 # print(sum(time_l)/len(time_l))
