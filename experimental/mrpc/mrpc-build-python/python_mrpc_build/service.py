513e4dfda03dce62be86e0c320754d9dea89d4a1
Arnav Jindal <arnav.jindal7@gmail.com>
2023-10-26 02:50:38 -0400

    start py build

---
+++ experimental/mrpc/mrpc-build-python/python_mrpc_build/service.py
@@ -0,0 +1,9 @@
+class Service:
+
+    def __init__(self, name: str, package: str, identifier: str, comments: list[str]) -> None:
+        self.name = name
+        self.package = package
+        self.identifier = identifier
+        self.comments = comments
+        pass
+    
